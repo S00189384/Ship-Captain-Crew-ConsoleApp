97a4d0341253277f648fae13176092b6ef3063d8
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2022-04-26 21:36:54 +0100

    Start

---
+++ Ship-Captain-Crew-Game/Ship-Captain-Crew-Game/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://www.dicegamedepot.com/ship-captain-and-crew-dice-game-rules/
+
+
+
+namespace Ship_Captain_Crew_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
